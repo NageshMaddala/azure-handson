45916c6890c7ad7a1fe7fba13cd988a807a529ed
NageshMaddala <nageshvmr@gmail.com>
2024-09-08 15:59:50 -0400

    Added boiler plate setup for Function service

---
+++ SampleFunction/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleFunction.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
