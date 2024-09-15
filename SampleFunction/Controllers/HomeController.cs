cfe897ece146f4e64a5b712efb137d152574b32c
NageshMaddala <nageshvmr@gmail.com>
2024-09-15 09:42:45 -0400

    Added boilerplate code for azure functions

--- SampleFunction/Controllers/HomeController.cs
+++ SampleFunction/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using SampleFunction.Models;
 using System.Diagnostics;
 
@@ -7,6 +8,7 @@ namespace SampleFunction.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		static readonly HttpClient client = new HttpClient();
 
 		public HomeController(ILogger<HomeController> logger)
 		{
@@ -16,6 +18,26 @@ namespace SampleFunction.Controllers
 		public IActionResult Index()
 		{
 			return View();
+		}
+
+		//http://localhost:7018/api/OnSalesUploadWriteToQueue
+
+		[HttpPost]
+		public async Task<IActionResult> Index(SalesRequest salesRequest)
+		{
+			salesRequest.Id = Guid.NewGuid().ToString();
+
+			using (var content = new StringContent(JsonConvert.SerializeObject(salesRequest),
+				System.Text.Encoding.UTF8, "application/json"))
+			{
+				// call our function and pass the content
+
+				HttpResponseMessage response = await client.PostAsync("http://localhost:7069/api/OnSalesUploadWriteToQueue", content);
+
+				string retValue = response.Content.ReadAsStringAsync().Result;
+			}
+
+			return RedirectToAction(nameof(Index));
 		}
 
 		public IActionResult Privacy()
